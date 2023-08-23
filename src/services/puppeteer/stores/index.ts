c3efadc553ed771bc4f4ec880f542a92932b501b
Enzo Bocalon <enzo.bocalon@hotmail.com>
2023-08-23 13:43:47 -0300

    feat: removed unused imports

--- src/services/puppeteer/stores/index.ts
+++ src/services/puppeteer/stores/index.ts
@@ -1,13 +1,5 @@
 import { Stores } from '../../../types/stores';
 import { Page } from 'puppeteer';
-import kabum from './kabum';
-import pichau from './pichau';
-import terabyteshop from './terabyteshop';
-import mercadolivre from './mercadolivre';
-import amazon from './amazon';
-import netshoes from './netshoes';
-import magazineluiza from './magazineluiza';
-import americanas from './americanas';
 
 const stores = {
 // storeName = (page: Page) => storeName(page)
